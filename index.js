c67194359fe56cd03b82a2a8933d9c3ac1605ebe
Genis Bardales <genis96@yahoo.com>
2021-01-17 16:10:00 -0500

    placed the questions inside the const questions and then worked on the function init and finction writeToFile inside the function init so that it shows up when i run it inside node.

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'What is your age?',
-            name: 'age'
-        },
-        {
-            type: 'input',
-            message: 'What is your favorite color?',
-            name: 'color'
-        }
-    ])
+// inquirer
+//     .prompt([
+//         {
+//             type: 'input',
+//             message: 'What is your age?',
+//             name: 'age'
+//         },
+//         {
+//             type: 'input',
+//             message: 'What is your favorite color?',
+//             name: 'color'
+//         }
+//     ])
 
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your age?',
+        name: 'age'
+    },
+    {
+        type: 'input',
+        message: 'What is your favorite color?',
+        name: 'color'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(resp => {
+        writeToFile(resp)
+    })
+}
 
 // Function call to initialize app
 init();
